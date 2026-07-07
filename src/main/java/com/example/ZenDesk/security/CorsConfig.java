277736c1be9d53c26b59522a166450c663a67428
BhanuBhanusha <bhanubhanusha379@gmail.com>
2026-07-07 19:25:58 +0530

    Update CorsConfig.java

--- src/main/java/com/example/ZenDesk/security/CorsConfig.java
+++ src/main/java/com/example/ZenDesk/security/CorsConfig.java
@@ -15,7 +15,7 @@ public class CorsConfig {
     public CorsConfigurationSource corsConfigurationSource()
     {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("https://BhanuBhanusha.github.io"));
+        config.setAllowedOrigins(List.of("https://bhanubhanusha.github.io"));
         config.setAllowedMethods(List.of("GET","POST","PUT","PATCH","DELETE","OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
